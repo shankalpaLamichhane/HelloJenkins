7838ddb27133c1d968bb3ae9f68fbed30f6a80cf
sankalpa <sankalpa.lamichhane@citytech.global>
2020-05-18 23:27:55 +0545

    build failure fixed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
 	public static void main(String a[]){
 		System.out.println("Hello World");
-        System.out.println("Next build pipeline in jenkins")
+        System.out.println("Next build pipeline in jenkins");
 	}
 }
